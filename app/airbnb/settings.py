b7f28fe840d187335aa04bab941c7511429d478a
simplifywoopii <simplifywoopii@gmail.com>
2024-08-29 17:46:05 +0900

    added cors header

--- app/airbnb/settings.py
+++ app/airbnb/settings.py
@@ -41,6 +41,7 @@ SYSTEM_APPS = [
     "django.contrib.staticfiles",
 ]
 THIRD_PARTY_APPS = [
+    "corsheaders",
     "rest_framework",
     "drf_yasg",
 ]
@@ -60,6 +61,7 @@ ROUTER_APPS = [
 INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + ROUTER_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -165,6 +167,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ["*"]
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
